6f52564870b17f8b386f4512d1bdfd0ab3853e7c
NFGarilli <garillinicolas@hotmail.com>
2021-04-22 03:35:55 -0300

    Se refactoriza el constructor

--- src/ar/edu/unq/po2/tp3/Punto.java
+++ src/ar/edu/unq/po2/tp3/Punto.java
@@ -7,18 +7,26 @@ public class Punto {
 	private float y;
 	public Punto(float x, float y) {
 		super();
-		this.x = x;
-		this.y = y;
+		this.setXY(x, y);
 	}
+	
+	public void setXY(float x, float y) {
+		this.setX(x);
+		this.setY(y);
+	}
+	
 	public float getX() {
 		return x;
 	}
+	
 	public void setX(float x) {
 		this.x = x;
 	}
+	
 	public float getY() {
 		return y;
 	}
+	
 	public void setY(float y) {
 		this.y = y;
 	}
